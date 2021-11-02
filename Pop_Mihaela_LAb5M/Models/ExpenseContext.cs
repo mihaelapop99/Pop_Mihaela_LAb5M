ec59fb16b5eb029ebdc0ec19de5d6fa94dd94b39
Mihaela Pop <mihaelapop919@gmail.com>
2021-11-02 19:59:16 +0200

    Add project files.

---
+++ Pop_Mihaela_LAb5M/Models/ExpenseContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Pop_Mihaela_LAb5M.Models;
+
+namespace Pop_Mihaela_LAb5M.Models
+{
+    public class ExpenseContext : DbContext
+    {
+        public ExpenseContext(DbContextOptions<ExpenseContext> options)
+        : base(options)
+        {
+        }
+        public DbSet<Expense> Expense { get; set; }
+        public DbSet<ExpenseDTO> ExpenseDTO { get; set; }
+        
+
+    }
+}
